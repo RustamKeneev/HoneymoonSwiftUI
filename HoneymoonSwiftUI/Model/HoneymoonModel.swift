b81a7c24429a1b37b32dd220ee54dd5fec78dc02
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-04-14 22:57:25 +0600

    added cardview

---
+++ HoneymoonSwiftUI/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  HoneymoonSwiftUI
+//
+//  Created by Rustam Keneev on 14/4/24.
+//
+
+import SwiftUI
+
+struct Destination{
+    var place: String
+    var country: String
+    var image: String
+}
