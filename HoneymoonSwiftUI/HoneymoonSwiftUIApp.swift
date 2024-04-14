3fd5c8a82b636b7bee66dc6561871fcc014a3d51
RustamKeneev <dr.rustamkeneev@gmail.com>
2024-04-14 16:10:51 +0600

    Initial Commit

---
+++ HoneymoonSwiftUI/HoneymoonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonSwiftUIApp.swift
+//  HoneymoonSwiftUI
+//
+//  Created by Rustam Keneev on 14/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
